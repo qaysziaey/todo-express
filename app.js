3dcdb8d80b09f19d902a807a52bb0d13bc9af7d2
Qais Ziaey <qziaey@gmail.com>
2024-02-21 14:10:38 +0100

    add vercel

--- app.js
+++ app.js
@@ -1,3 +1,71 @@
 const express = require("express");
+const postgres = require("@vercel/postgres");
+const cors = require("cors");
 const app = express();
-const postgres = require("postgres");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", async (request, response) => {
+  createNotes();
+  //table was created => load data
+  const { rows } = await postgres.sql`SELECT * FROM todos`;
+  return response.json(rows);
+});
+
+app.get("/:id", async (request, response) => {
+  createNotes();
+  //table was created => load data
+  const { id } = request.params;
+  const { rows } = await postgres.sql`SELECT * FROM todos WHERE id = ${id}`;
+
+  if (!rows.length) {
+    return response.json({ error: "Note not found." });
+  }
+
+  return response.json(rows[0]);
+});
+
+app.post("/", async (request, response) => {
+  createNotes();
+  const { content } = JSON.parse(request.body);
+  if (content) {
+    await postgres.sql`INSERT INTO todos (content) VALUES (${content})`;
+    response.json({ message: "Successfully created note." });
+  } else {
+    response.json({ error: "Note NOT created. Content is missing." });
+  }
+});
+
+/* vegan delete route */
+app.delete("/:tofu", async (request, response) => {
+  createNotes();
+  /* const  tofu  = request.params.tofu; */
+  const { tofu } = request.params;
+  const { rowCount } = await postgres.sql`DELETE FROM todos WHERE id = ${tofu}`;
+
+  if (!rowCount) {
+    return response.json({ error: "Note not found." });
+  }
+
+  response.json({ message: "Successfully deleted note." });
+});
+
+// default catch-all handler
+app.get("*", (request, response) => {
+  response.status(404).json({ message: "route not defined" });
+});
+
+module.exports = app;
+
+/*
+ * - we want to create a new table called notes
+ * - from within our code
+ */
+async function createNotes() {
+  await postgres.sql`CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        content VARCHAR(255)
+    )`;
+}
